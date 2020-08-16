860defaa96a565487671899803eb4e5441cc41f4
vagrant <vagrant@localhost.localdomain>
2020-08-16 02:52:39 +0000

    add:genre to model and coutroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+  get 'genres/edit'
   root 'homes#top'
   devise_for :admins
   devise_for :customers
   get "homes/about" => "homes#about"
   resources :shipping_addresses, only: [:index, :edit]
+  resources :genres, only: [:index, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
