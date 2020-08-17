c3f5f4ee1869af4ea4394bdf27141ceb69087692
vagrant <vagrant@localhost.localdomain>
2020-08-17 14:21:16 +0000

    add_customers_view

--- app/controllers/end_user/customers_controller.rb
+++ app/controllers/end_user/customers_controller.rb
@@ -1,10 +1,11 @@
 class EndUser::CustomersController < ApplicationController
   def show
+  	@customers = Customer.find_by(current_customer)
   end
 
   def edit
   end
 
-  def exit
+  def exit_page
   end
 end
