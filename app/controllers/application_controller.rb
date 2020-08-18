3d3bbb1a68655926d0942381ba365c361f3f3d88
vagrant <vagrant@localhost.localdomain>
2020-08-18 02:24:41 +0000

    update:admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+	befor_action :authenticate_adnim!
+	def after_sign_in_path_for(resource)
+		admin_homes_top_path   #sing_in後のパス
+	end
+
+	def after_sign_out_path_for(resource)
+		new_admin_session_path  #sing_out後のパス
+	end
 end
