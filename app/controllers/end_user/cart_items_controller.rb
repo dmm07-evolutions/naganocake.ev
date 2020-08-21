c5a96faf79f77f6ea8ac36c77e24f25080022475
vagrant <vagrant@localhost.localdomain>
2020-08-21 08:31:08 +0000

    fix:cart_item_and_add_comment

--- app/controllers/end_user/cart_items_controller.rb
+++ app/controllers/end_user/cart_items_controller.rb
@@ -1,28 +1,45 @@
 class EndUser::CartItemsController < ApplicationController
 
+#ショッピングカートページ
   def index
-  	@cart_item = CartItem.find_by(customer_id: current_customer)
+    #ログイン中の顧客のカートない商品を取得
+  	@cart_item = CartItem.where(customer_id: current_customer)
   end
 
+#アイテム詳細ページのカートに入れるボタンを押したら実行される。
   def create
+    # 新しいインスタンスの定義
   	@cart_item = CartItem.new
+    # @cart_itemのcustomer_idにログイン中の顧客のIDを代入
   	@cart_item.customer_id = current_customer.id
+    #@cart_itemに紐付いているitemの定義（@cart_itemのitem_idにparamsに受け取ったidを代入)
   	@cart_item.item = Item.find(params[:id])
+    #@cart_itemのpuantityに数量を代入
   	@cart_item.quantity = 1
+    #@cart_itemをデータベースに保存する
   	@cart_item.save
-
-  	# if @cart_item.blank?
-  	# 	@cart_item = current_cart.cart_items.build(item_id: params[:id])
-  	# end
-  	# @cart_item.quantity += params[:quantity].to_i
-  	# @cart_item.save
-  	 redirect_to items_path
+    #商品一覧ページに移行
+  	redirect_to items_path
   end
 
-  def destory
+#ショッピングカートページの削除ボタンを押したら実行される
+  def destroy
+    #選択したカート内商品のデータを受け取る
   	@cart_item = CartItem.find(params[:id])
+    #受け取ったカートない商品のデータを削除する
   	@cart_item.destroy
+    #ショッピングカートページに移行する
   	redirect_to cart_items_path
+  end
+
+#カートをからにするボタンを押すと実行される
+  def destroy_all
+    #ログイン中の顧客のカート内商品全てを取得
+    @cart_item = CartItem.where(customer_id: current_customer)
+    #全て削除
+    @cart_item.destroy_all
+    #ショッピングカートページに移行
+    redirect_to cart_items_path
   end
 
   private
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,4 +2,16 @@ class CartItem < ApplicationRecord
 	# customerのアソシエーション
 	belongs_to :customer
 	belongs_to :item
+
+	#消費税込みの単価
+	def price_tax
+		price_tax = item.price.to_i * 1.08
+		price_tax.round
+	end
+
+	#消費税込みの小計（単価xx数量）
+	def subtotal
+		subtotal = price_tax.to_i * quantity
+	end
+
 end
