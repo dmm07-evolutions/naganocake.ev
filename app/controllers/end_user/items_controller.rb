ff4863654b1c9adfbee6bb2d96d0ab85a6981107
vagrant <vagrant@localhost.localdomain>
2020-08-21 11:33:23 +0000

    add:coment

--- app/controllers/end_user/items_controller.rb
+++ app/controllers/end_user/items_controller.rb
@@ -5,7 +5,10 @@ class EndUser::ItemsController < ApplicationController
 
   def show
   	@item = Item.find(params[:id])
+
   	@cart_item = CartItem.new
   end
 
 end
+
+# praiveate等は追加しなくても他のところから取れたので動いた
