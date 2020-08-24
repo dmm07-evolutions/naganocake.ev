9509b70041d0d5edeb67bdd520c247d39d4a1732
vagrant <vagrant@localhost.localdomain>
2020-08-24 11:07:50 +0000

    layout_header

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
 	belongs_to :customer
-	has_many :order_items, dependent: :destroy
+	has_many :ordered_items, dependent: :destroy
 
 	 enum payment_method:    [ :クレジットカード決済, :銀行振り込み ]
 	# enum address_select:    [ :ご自身の住所, :ご登録済みの住所, :新しいお届け先 ]
--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_134154) do
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
-  
+
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
     t.boolean "is_active", default: true
