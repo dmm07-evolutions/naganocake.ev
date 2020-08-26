d1d24a33a1b6819c43d80668ceaf4cfcba216d82
vagrant <vagrant@localhost.localdomain>
2020-08-26 18:14:41 +0000

    change:view

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
 	has_many :ordered_items, dependent: :destroy
 
 	 enum payment_method: [ :クレジットカード決済, :銀行振り込み ]
-	 #enum status: [ :入金待ち, :入金確認, :製作中, :発送準備中, :発送済み ]
+	 enum status: [ :入金待ち, :入金確認, :製作中, :発送準備中, :発送済み ]
 
    #追加属性の定義
    attr_accessor :address_select
