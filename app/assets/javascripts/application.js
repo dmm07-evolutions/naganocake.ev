cd174ede091195219d2a824bb501de772f7d9401
vagrant <vagrant@localhost.localdomain>
2020-08-28 08:30:55 +0000

    add_gorilla-icon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,16 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+
+
+
+//ゴリラ往復
+$(document).ready(function(){
+    $('.gorilla_wrap').on('animationend', function () {
+        if ($(this).attr('data-order') === 'left') {
+            $(this).attr('data-order', 'right');
+        } else {
+            $(this).attr('data-order', 'left');
+        }
+    });
+});
